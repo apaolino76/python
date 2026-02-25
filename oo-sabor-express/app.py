707de4810e82aab401d5ca05d9fc8596890c4264
angelo.paolino <angelo.paolino@dataprev.gov.br>
2026-02-25 19:17:47 -0300

    inclusao do projeto oo-sabor-express

---
+++ oo-sabor-express/app.py
@@ -0,0 +1,31 @@
+from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
+
+restaurante_praca = Restaurante('praça', 'Gourmet')
+
+bebida_suco = Bebida('Suco de Melancia', 5.0, 'grande')
+bebida_suco.aplicar_desconto()
+
+prato_paozinho = Prato('Pãozinho', 2.0, 'O melhor pão da cidade')
+prato_paozinho.aplicar_desconto()
+
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
+
+# restaurante_praca.receber_avaliacao('Gui', 10)
+# restaurante_praca.receber_avaliacao('Lais', 8)
+# restaurante_praca.receber_avaliacao('Emy', 5)
+
+# restaurante_mexicano = Restaurante('mexican food', 'Mexicana')
+# restaurante_mexicano.alternar_estado()
+# restaurante_japones = Restaurante('Japa', 'Japonesa')
+
+def main():
+    # Restaurante.listar_restaurantes()
+    # print(bebida_suco)
+    # print(prato_paozinho)
+    restaurante_praca.exibir_cardapio
+
+if __name__ == '__main__':
+    main()
